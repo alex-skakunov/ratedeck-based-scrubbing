f9ff2560771b7bb4dd396d3fca61a565179841eb
Alexander Skakunov <askakunov@itera-research.com>
2018-10-01 14:39:24 +0300

    [Scrubbing] fixed a case when no file is uploaded

--- actions/controllers/scrubbing.php
+++ actions/controllers/scrubbing.php
@@ -18,7 +18,7 @@ $areacodes_all = empty($_POST['areacode']);
 
 $recordset = query('SELECT * FROM `queue` ORDER BY `id` DESC')->fetchAll(PDO::FETCH_ASSOC);
 
-$theLastQueuedItem = query('SELECT * FROM `queue` ORDER BY `id` DESC LIMIT 1')->fetch(PDO::FETCH_ASSOC);
+$theLastQueuedItem = query('SELECT * FROM `queue` WHERE `filename` <> "" AND `filename` IS NOT NULL ORDER BY `id` DESC LIMIT 1')->fetch(PDO::FETCH_ASSOC);
 
 
 $_areacodeList = query('SELECT DISTINCT `region` FROM `areacode` ORDER BY `region` ASC')->fetchAll(PDO::FETCH_ASSOC);
@@ -48,13 +48,15 @@ if (!in_array($blacklistType, array('lawsuits', 'master'))) {
     return $message = 'Please choose a DNC list to upload to';
 }
 
+$isTheOnlyFile = sizeof($_FILES['file_source']['name']) == 1;
+
 foreach ($_FILES['file_source']['name'] as $index => $filename) {
     $errorCode = $_FILES['file_source']['error'][$index];
-    if (0 == $_FILES['file_source']['size'][$index]) {
+    if (!$isTheOnlyFile && 0 == $_FILES['file_source']['size'][$index]) {
         $errorCode = -1; //empty file
     }
 
-    if (UPLOAD_ERR_OK != $errorCode) {
+    if (!$isTheOnlyFile && UPLOAD_ERR_OK != $errorCode) {
         $errorMessage = $uploadErrors[$errorCode];
         query('INSERT INTO `queue`(`filename`, `temp_filename`, `max_price`, `include_wireless_type`, `include_landline_type`, `specific_states_list`, `blacklist_type`, `status`, `error_message`, `created_at`) VALUES (
             :original_filename,
@@ -83,59 +85,61 @@ foreach ($_FILES['file_source']['name'] as $index => $filename) {
     }
 
     $temp_file = $_FILES['file_source']['tmp_name'][$index];
-    $our_file  = tempnam(TEMP_DIR, 'scrub');
-    if ( !move_uploaded_file( $temp_file, $our_file ) ) {
-      $error = 'Could not copy [' . $temp_file .'] to [' . $our_file . ']';
-      return;
+    if (is_uploaded_file($temp_file)) {
+        $our_file  = tempnam(TEMP_DIR, 'scrub');
+        if ( !move_uploaded_file( $temp_file, $our_file ) ) {
+          $error = 'Could not copy [' . $temp_file .'] to [' . $our_file . ']';
+          return;
+        }
+
+        $zip = new ZipArchive;
+        if ($zip->open($our_file) === TRUE) {
+            $csvFilename = $zip->getNameIndex(0);
+            $zip->extractTo(TEMP_DIR, array($csvFilename));
+            $zip->close();
+            rename(TEMP_DIR . $csvFilename, $our_file);
+        }
     }
 
-    $zip = new ZipArchive;
-    if ($zip->open($our_file) === TRUE) {
-        $csvFilename = $zip->getNameIndex(0);
-        $zip->extractTo(TEMP_DIR, array($csvFilename));
-        $zip->close();
-        rename(TEMP_DIR . $csvFilename, $our_file);
+    if (!empty($_FILES['file_source']['name'][$index])) {
+        $originalFilename = $_FILES['file_source']['name'][$index];
+    } elseif (!empty($theLastQueuedItem)) {
+        $originalFilename = $theLastQueuedItem['filename'];
+    } else {
+      throw new Exception("No file was uploaded");
     }
 
-  if (!empty($_FILES['file_source']['name'][$index])) {
-      $originalFilename = $_FILES['file_source']['name'][$index];
-  } elseif (!empty($theLastQueuedItem)) {
-      $originalFilename = $theLastQueuedItem['filename'];
-  } else {
-    throw new Exception("No file was uploaded");
-  }
+    if (!empty($our_file)) {
+        $temporaryFilename = pathinfo($our_file, PATHINFO_BASENAME);
+    } elseif (!empty($theLastQueuedItem)) {
+        $temporaryFilename = $theLastQueuedItem['temp_filename'];
+    }
 
-  if (!empty($our_file)) {
-      $temporaryFilename = pathinfo($our_file, PATHINFO_BASENAME);
-  } elseif (!empty($theLastQueuedItem)) {
-      $temporaryFilename = $theLastQueuedItem['temp_filename'];
-  }
+    $rows_count = !empty($theLastQueuedItem) ? $theLastQueuedItem['rows_count'] : null;
 
-  $rows_count = !empty($theLastQueuedItem) ? $theLastQueuedItem['rows_count'] : null;
-
-  query('INSERT INTO `queue`(`filename`, `temp_filename`, `max_price`, `include_wireless_type`, `include_landline_type`, `specific_states_list`, `blacklist_type`, `rows_count`, `created_at`) VALUES (
-      :original_filename,
-      :temp_filename,
-      :max_price,
-      :include_wireless_type,
-      :include_landline_type,
-      :specific_states_list,
-      :blacklist_type,
-      :rows_count,
-      NOW()
-  )', array(
-      ':original_filename' => $originalFilename,
-      ':temp_filename' => $temporaryFilename,
-      ':max_price' => (float)$_POST['max_price'],
-      ':include_wireless_type' => (int)$_POST['wireless'],
-      ':include_landline_type' => (int)$_POST['landline'],
-      ':specific_states_list' => !empty($areacodes) 
-        ? implode(',', $areacodes)
-        : null,
-      ':blacklist_type' => $blacklistType,      
-      ':rows_count' => $rows_count
-  ));
+    query('INSERT INTO `queue`(`filename`, `temp_filename`, `max_price`, `include_wireless_type`, `include_landline_type`, `specific_states_list`, `blacklist_type`, `rows_count`, `created_at`) VALUES (
+        :original_filename,
+        :temp_filename,
+        :max_price,
+        :include_wireless_type,
+        :include_landline_type,
+        :specific_states_list,
+        :blacklist_type,
+        :rows_count,
+        NOW()
+    )', array(
+        ':original_filename' => $originalFilename,
+        ':temp_filename' => $temporaryFilename,
+        ':max_price' => (float)$_POST['max_price'],
+        ':include_wireless_type' => (int)$_POST['wireless'],
+        ':include_landline_type' => (int)$_POST['landline'],
+        ':specific_states_list' => !empty($areacodes) 
+          ? implode(',', $areacodes)
+          : null,
+        ':blacklist_type' => $blacklistType,      
+        ':rows_count' => $rows_count
+    ));
 }
 
 $recordset = query('SELECT * FROM `queue` ORDER BY `id` DESC')->fetchAll(PDO::FETCH_ASSOC); //refetch the fresh copy
-$theLastQueuedItem = query('SELECT * FROM `queue` ORDER BY `id` DESC LIMIT 1')->fetch(PDO::FETCH_ASSOC);
+$theLastQueuedItem = query('SELECT * FROM `queue` WHERE `filename` <> "" AND `filename` IS NOT NULL ORDER BY `id` DESC LIMIT 1')->fetch(PDO::FETCH_ASSOC);
