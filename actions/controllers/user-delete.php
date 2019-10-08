f93b67410b876ea34b403170de4ccbaec733e789
Alexander Skakunov <alexander.skakunov@perfectial.com>
2019-10-08 10:09:10 +0100

    user delete fix

--- actions/controllers/user-delete.php
+++ actions/controllers/user-delete.php
@@ -6,14 +6,15 @@ if (empty($_POST)) {
   return;
 }
 
-$userId = $_SESSION['user']['id'];
+$userId = (int)$_POST['id'];
+$currentUserId = $_SESSION['user']['id'];
 
-erase_user_queue($userId);
-if ($userId == $_POST['id']) {
+erase_user_queue($currentUserId);
+if ($currentUserId == $userId) {
   return $errorMessage = 'You cannot kill yourself.';
 }
 
-query('DELETE FROM user WHERE `id` = ' . (int)$_POST['id']);
+query('DELETE FROM `user` WHERE `id` = ' . $userId);
 query("DROP TABLE IF EXISTS `blacklist_user_$userId`");
 
 header('HTTP/1.0 204 No Content');
